23120717d7165071cd1a290605a1648c8926c518
tibo <kevfyrezz@gmail.com>
2024-11-09 11:49:07 +0000

    first commit readme + mise en place

---
+++ README.md
@@ -0,0 +1,10 @@
+# TautoRep 
+
+
+## Description :
+
+TautoRep est une extension firefox permetant de sauvegardé des templates de réponse
+
+---
+
+##### version 0.0.1
---
+++ background_script.js
@@ -0,0 +1,19 @@
+console.log('ok');
+
+browser.contextMenus.create(
+    {
+      id: "log-selection",
+      title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
+      contexts: ["selection"],
+    },
+    onCreated,
+  );
+  
+  browser.contextMenus.onClicked.addListener(function(info, tab) {
+    switch (info.menuItemId) {
+      case "log-selection":
+        console.log(info.selectionText);
+        break;
+    }
+  })
+  
